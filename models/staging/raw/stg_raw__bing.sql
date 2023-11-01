3b6f87056f787f2397bf5cce5f4805e0eb61f95b
Manon Delespierre <manon.delespierre@gmail.com>
2023-11-01 13:13:27 +0000

    description int_campaign.sql + primary tests

--- models/staging/raw/stg_raw__bing.sql
+++ models/staging/raw/stg_raw__bing.sql
@@ -11,7 +11,7 @@ renamed as (
     select
         date_date,
         paid_source,
-        CAST(campaign_key as INT64) as campaign_key,
+        campaign_key,
         campgn_name as campaign_name,
         CAST(ads_cost as FLOAT64) as ads_cost,
         click,
