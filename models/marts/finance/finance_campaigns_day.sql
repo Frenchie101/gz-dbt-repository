4589f943ce9cc7f40e71ffe3a2550b56e2a2df2e
Manon Delespierre <manon.delespierre@gmail.com>
2023-11-01 15:25:46 +0000

    finance_campaigns_day is built

--- models/marts/finance/finance_campaigns_day.sql
+++ models/marts/finance/finance_campaigns_day.sql
@@ -1,16 +1,8 @@
 select 
-    fd.date_date
-    , ROUND(fd.total_operational_margin - ads_cost,2) as ads_margin
-    , fd.average_basket
-    , fd.nb_transactions
-    , fd.revenue
-    , fd.margin
-    , fd.total_operational_margin
-    , icd.ads_cost
-
+    *
+    , ROUND(fd.total_operational_margin - icd.ads_cost,2) as ads_margin
 from
 {{ref('int_campaigns_day')}} as icd
-LEFT JOIN
+JOIN
 {{ref('finance_days')}} as fd
 using (date_date)
-order by date_date
